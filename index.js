91f77dba857e4284ee2a4bce1b9f138eb80f6f1a
Mike <mike.watling@nexusmods.com>
2023-06-28 11:54:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 const path = require('path');
 const { fs, util, log } = require('vortex-api');
-const winapi = require('winapi-bindings');
-const GAME_ID = 'genshinimpact';
-const EPIC_ID = '41869934302e4b8cafac2d3c0e7c293d';
-const MOD_FILE_EXT = ".ini";
-const GIMI_PATH = path.join(util.getVortexPath('localAppData'), 'GenshinImpactMods');
-const GIMI_MODS_PATH = path.join(GIMI_PATH, '3dmigoto', 'Mods');
-const GIMI_LOADER = '3DMigoto Loader.exe';
+const { GAME_ID, EPIC_ID, GIMI_PATH, GIMI_MODS_PATH, GIMI_LOADER, findGame, findLauncher } = require('./common.js');
+const { testGIMI, installGIMI, testGIMIMod, installGIMIMod, testGIMIModType } = require('./installers.js');
 
 const tools = [
   {
@@ -46,47 +41,6 @@ function requiresLauncher(gamePath, store) {
   }
 }
 
-async function findLauncher() {
-  // To find where the game is installed we first need to find the launcher. It will either be installed via EGS or manually. 
-  try {
-    // Check EGS
-    const epic = await util.GameStoreHelper.findByAppId([EPIC_ID]);
-    return epic.gamePath;
-  }
-  catch(err) {
-    // Check for the standalone launcher
-    log('debug', 'Genshin Impact not found on Epic, checking for it\'s own launcher');
-    const instPathLauncher = winapi.RegGetValue(
-      'HKEY_LOCAL_MACHINE',
-      'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Genshin Impact',
-      'InstallPath');
-    if (!instPathLauncher) return;
-    else return instPathLauncher.value;
-  }
-}
-
-async function findGame() {
-  // To find where the game is installed we first need to find the launcher. It will either be installed via EGS or manually. 
-  const launcherPath = await findLauncher();
-  if (!launcherPath) return;
-
-  // Now we know where the launcher is we need to look at the config.ini to find the actual game.
-  const launcherConfig = path.join(launcherPath, 'config.ini');
-  try {
-    const configData = await fs.readFileAsync(launcherConfig, { encoding: 'utf8' });
-    const installPath = configData.split(`\n`)
-      .find(r => r.toLowerCase().startsWith('game_install_path'));
-    if (!installPath) throw new Error('game_install_path missing from Genshin Launcher config.ini');
-    const gameFolder = installPath.replace('game_install_path=', '').trim();
-    return gameFolder;
-  }
-  catch(err2) {
-    log('warn', 'Error locating Genshin Impact', err2);
-    return;
-  }
-
-}
-
 function main(context) {
   context.registerGame({
     id: GAME_ID,
@@ -101,6 +55,7 @@ function main(context) {
     requiredFiles: [
       'GenshinImpact.exe',
     ],
+    getGameVersion,
     requiresLauncher,
     environment: {},
     details: {
@@ -117,15 +72,17 @@ function main(context) {
   return true;
 }
 
-async function testGIMIModType(instructions) {
-  const GIMI_EXE = instructions.find(i => i.type === 'copy' && i.destination.toLowerCase().endsWith(GIMI_LOADER.toLowerCase()));
-  if (!!GIMI_EXE) return true;
-
-  // If there's an INI file, we can assume it's a GIMI mod.
-  const INI_FILE = instructions.find(i => i.type === 'copy' && path.extname(i.destination) === MOD_FILE_EXT);
-  if (!!INI_FILE) return true;
-
-  else return false;
+async function getGameVersion(gamePath) {
+  const gameConfigPath = path.join(gamePath, 'config.ini');
+  try {
+    const gameConfig = await fs.readFileAsync(gameConfigPath, { encoding: 'utf8' });
+    const versionLine = gameConfig.split('\n').find(l => l.toLowerCase().includes('game_version='));
+    return versionLine.replace('game_version=', '').trim();
+  }
+  catch(err) {
+    log('warn', 'Could not determine version for Genshin Impact', err);
+    throw new Error(`failed to parse ${gameConfigPath}`);
+  }
 
 }
 
@@ -151,114 +108,6 @@ function checkForGIMI(api, GIMI_PATH) {
         ],
       });
     });
-}
-
-
-function testGIMI(files, gameId) {
-  // See if we're installing GIMI by checking the EXE is in the archive.
-  let supported = (gameId === GAME_ID) &&
-    (files.find(file => path.basename(file).toLowerCase() === GIMI_LOADER.toLowerCase())!== undefined);
-
-  return Promise.resolve({
-    supported,
-    requiredFiles: [],
-  });
-}
-
-async function installGIMI(files, destinationPath) {
-  const modFile = files.find(file => path.basename(file).toLowerCase() === GIMI_LOADER.toLowerCase());
-  const rootPath = path.dirname(modFile);
-
-  const D3DX = path.join(rootPath, 'd3dx.ini');
-
-  const D3DXTempPath = path.join(destinationPath, D3DX);
-
-  let instructions = [];
-
-  try {
-    const fileData = await fs.readFileAsync(D3DXTempPath, { encoding: 'utf8' });
-    const gamePath = path.join((await findGame()), 'GenshinImpact.exe');
-    const newD3DX = addLaunchToINI(fileData, gamePath);
-
-    instructions.push({
-      type: 'generatefile',
-      data: Buffer.from(newD3DX, 'utf8'),
-      destination: D3DX,
-    });
-  }
-  catch(err) {
-    alert('Error updating D3DX file: '+err);
-  }
-  
-  // Remove directories and anything that isn't in the rootPath.
-  const filtered = files.filter(file => 
-    ((file.indexOf(rootPath) !== -1) 
-    && (!file.endsWith(path.sep)))).filter(f => !f.toLowerCase().endsWith('d3dx.ini'));
-
-  const copyInstructions = filtered.map(file => {
-    return {
-      type: 'copy',
-      source: file,
-      destination: file,
-    };
-  });
-
-  return Promise.resolve({ instructions: instructions.length ? [...copyInstructions, ...instructions] : copyInstructions });
-}
-
-function addLaunchToINI(currentINIdata, gameExePath) {
-  // The line we want to add
-  const launchLine = `launch = ${gameExePath}`;
-  // Split into lines, trim off any wierd whitespace.
-  const lines = currentINIdata.split('\n').map(l => l.trim());
-  // Find the [Loader] heading line number
-  let loaderIdx = lines.findIndex(l => l.toLowerCase().startsWith('[loader]'));
-  //The loader heading doesn't exist so we'll just add it to the top.
-  if (loaderIdx === -1) return [`[Launcher]`, launchLine, ...lines].join('\n');
-  // Check to see if the launch line exists
-  let launchIdx = lines.findIndex((line, index) => index >= loaderIdx && line.toLowerCase().startsWith('launch = '));
-  // if we can find it, replace it
-  if (launchIdx !== -1) {
-    lines[launchIdx] = launchLine;
-  }
-  // otherwise insert it below the header
-  else {
-    lines.splice(loaderIdx + 1, 0, launchLine);
-  }
-  return lines.join('\n');
-}
-
-function testGIMIMod(files, gameId) {
-  // Make sure we're able to support this mod.
-  let supported = (gameId === GAME_ID) &&
-    (files.find(file => path.extname(file).toLowerCase() === MOD_FILE_EXT)!== undefined);
-
-  return Promise.resolve({
-    supported,
-    requiredFiles: [],
-  });
-}
-
-function installGIMIMod(files) {
-  // The .ini file is expected to always be positioned in the mods directory we're going to disregard anything placed outside the root.
-  const modFile = files.find(file => path.extname(file).toLowerCase() === MOD_FILE_EXT);
-  const idx = modFile.indexOf(path.basename(modFile));
-  const rootPath = path.dirname(modFile);
-  
-  // Remove directories and anything that isn't in the rootPath.
-  const filtered = files.filter(file => 
-    ((file.indexOf(rootPath) !== -1) 
-    && (!file.endsWith(path.sep))));
-
-  const instructions = filtered.map(file => {
-    return {
-      type: 'copy',
-      source: file,
-      destination: path.join('3dmigoto', 'Mods', file),
-    };
-  });
-
-  return Promise.resolve({ instructions });
 }
 
 module.exports = {
